d56b7bfa434c424b5a2c5dfd6d6e5823d2ce5f8c
임은호 <>
2023-04-26 08:35:36 +0900

    First Commit all files

---
+++ routes/carts.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const Cart = require("../schemas/cart.js")
+const Goods = require("../schemas/goods.js")
+
+// localhost:3000/api/carts  GET Method
+router.get("/carts", async(req,res)=>{
+    const carts = await Cart.find({});
+    // [
+    //   {goodsId, quantity}
+    // ]
+    const goodsIds = carts.map((cart)=>{
+        return cart.goodsId;
+    })
+    // [2, 11, 19]
+    const goods = await Goods.find({goodsId: goodsIds})
+    // Goods에 해당하는 모든 정보를 가지고 올건데, 
+    // goodsIds 변수 안에 존재하는 값일 때에만 조회하라.
+    // Goods 정보중 cart에 들어가 있는 요소만 가져와라.
+
+    const results = carts.map((cart)=>{
+        return {
+            "quantity": cart.quantity,
+            "goods": goods.find((item)=> item.goodsId === cart.goodsId)
+        }
+    })
+    res.json({
+        "carts": results,
+    })
+})
+
+module.exports = router
